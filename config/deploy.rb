8db907f8ff42b63222400e74572dcb2a9a3f1d0f
Asep Bagja <bepitulaz@gmail.com>
2014-07-09 01:32:40 +0700

    add puma recipe for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,8 +34,36 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+set :stage, 'production'
+set :shared_children, shared_children << 'tmp/sockets'
+ 
+puma_sock    = "unix://#{shared_path}/sockets/puma.sock"
+puma_control = "unix://#{shared_path}/sockets/pumactl.sock"
+puma_state   = "#{shared_path}/sockets/puma.state"
+puma_log     = "#{shared_path}/log/puma-#{stage}.log"
+
 namespace :deploy do
 
+  desc "Start the application"
+  task :start do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec puma -b '#{puma_sock}' -e #{stage} -t2:4 --control '#{puma_control}' -S #{puma_state} >> #{puma_log} 2>&1 &", :pty => false
+  end
+ 
+  desc "Stop the application"
+  task :stop do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} stop"
+  end
+ 
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} restart"
+  end
+ 
+  desc "Status of the application"
+  task :status, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} stats"
+  end
+  
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
