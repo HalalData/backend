9e47fcaf9f02c46e7fbe1880f5b42ec414c5c47b
Asep Bagja <bepitulaz@gmail.com>
2014-07-09 04:30:01 +0700

    setting mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@halaldata.org'}
 end
