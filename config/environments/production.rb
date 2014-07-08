c4c38c99f81c5834fda15a660d335cb9ed2e5bbb
Asep Bagja <bepitulaz@gmail.com>
2014-07-09 05:07:30 +0700

    add mail host
    change to google smtp
    
    add host for mailer
    
    fixing the host: syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'halaldata.org' }
   config.action_mailer.delivery_method = :sendmail
-
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@halaldata.org'}
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 end
