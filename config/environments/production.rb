93bc1c061ea8fe092eda897081029b32c9c8bbd1
Asep Bagja <bepitulaz@gmail.com>
2014-07-09 11:43:46 +0700

    fixing the mailer for registration process

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default_options = {from: 'no-reply@halaldata.org'}
 end
