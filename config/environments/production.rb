6faa427e693538b612e234e591add51ec8a4ef42
Asep Bagja <bepitulaz@gmail.com>
2014-07-15 11:04:19 +0700

    add google analytic and mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'halaldata.org' }
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "mailgun.org",
+    :port => 587,
+    :domain => "mailgun.org",
+    :user_name => "postmaster@asep.co.mailgun.org",
+    :password => "70veae5vjj-7"
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@halaldata.org'}
