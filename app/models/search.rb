949121845609e33aca3bb96ad4330faecaae0526
Asep Bagja <bepitulaz@gmail.com>
2014-07-17 01:38:24 +0700

    add source field to model

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search < ActiveRecord::Base
 	# @param query is a string to be searched
 	# @return array
 	def self.full_text_search(query, page)
-		sql = "SELECT id, name, address, postal_code, category_name, country_name, "
+		sql = "SELECT id, name, address, postal_code, category_name, country_name, source, "
   	sql << "MATCH(name, address, category_name, country_name) AGAINST('#{query}') AS score "
   	sql << "FROM searches WHERE MATCH(name, address, category_name, country_name) AGAINST('#{query}') ORDER BY score DESC;"
 	
