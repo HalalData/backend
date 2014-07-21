e9a09e9f59cd58e99c2fb7f6021f74c500f1f1d7
Asep Bagja <bepitulaz@gmail.com>
2014-07-21 12:50:26 +0700

    preparing the map from 4sq

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -13,6 +13,17 @@ class PlaceController < ApplicationController
 
   def show
     @Place = Index.find(params[:id])
+
+    # check from 4sq
+    if @Place.city_name
+      client = Foursquare2::Client.new(
+        client_id: 'ZS2RFX0CSAP3KIV1DYM24OKVZXMGHIJ1SNOAO1ANJOBIQOJ3', 
+        client_secret: 'YYYMXUT51CJ02Y2SFCKTRFPVQ1HESWCAVOGVBWQNXUZUR0TS',
+        api_version: '20140701'
+        )
+
+      @Venue = client.search_venues(ll: @Place.city_latitude.to_s + ',' + @Place.city_longitude.to_s, query: @Place.name)
+    end
   end
 
   def update
